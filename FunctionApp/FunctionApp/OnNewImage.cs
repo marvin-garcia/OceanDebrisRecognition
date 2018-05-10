440c6e3c91bc3f663aff565b4c0fe6bf92e74136
Marvin Garcia <magar@microsoft.com>
2018-05-10 18:04:26 -0500

    Updated logic app template and included it in the main flow

--- FunctionApp/FunctionApp/OnNewImage.cs
+++ FunctionApp/FunctionApp/OnNewImage.cs
@@ -1,34 +1,62 @@
+using System;
+using System.Configuration;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
 
-namespace FunctionApp
+namespace AerialObjectRecognitionFunction
 {
     public static class OnNewImage
     {
+        private static TelemetryClient telemetryClient = new TelemetryClient(
+            new Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration(
+                ConfigurationManager.AppSettings["APPINSIGHTS_INSTRUMENTATIONKEY"]));
+
         [FunctionName("OnNewImage")]
         public static async Task<object> Run([HttpTrigger(WebHookType = "genericJson")]HttpRequestMessage req, TraceWriter log)
         {
-            log.Info($"Webhook was triggered!");
-
-            string jsonContent = await req.Content.ReadAsStringAsync();
-            dynamic data = JsonConvert.DeserializeObject(jsonContent);
-
-            if (data.first == null || data.last == null)
+            try
             {
-                return req.CreateResponse(HttpStatusCode.BadRequest, new
+                log.Info($"Webhook was triggered!");
+
+                // Get request body
+                dynamic data = await req.Content.ReadAsAsync<object>();
+                log.Info(data.ToString());
+
+                // Get image url
+                string imagePath = data.path;
+                string imageUrl = $"https://{ConfigurationManager.AppSettings["StorageAccountName"]}.blob.core.windows.net{imagePath}";
+
+                log.Info(imageUrl);
+
+                // Get image prediction results
+                var predictionResult = await ImagePredictor.PredictImageUrl(imageUrl);
+
+                // Validate image
+                bool objectFound = ImagePredictor.ValidateImagePrediction(predictionResult);
+                Models.Output output = new Models.Output()
                 {
-                    error = "Please pass first/last properties in the input object"
-                });
-            }
+                    ObjectFound = objectFound,
+                    ImageUrl = imageUrl,
+                };
 
-            return req.CreateResponse(HttpStatusCode.OK, new
+                // Track event on Application Insights
+                if (objectFound)
+                    telemetryClient.TrackEvent("Object");
+                else
+                    telemetryClient.TrackEvent("Oean");
+
+                // return result
+                return req.CreateResponse(HttpStatusCode.OK, output);
+            }
+            catch (Exception e)
             {
-                greeting = $"Hello {data.first} {data.last}!"
-            });
+                throw e;
+            }
         }
     }
 }
